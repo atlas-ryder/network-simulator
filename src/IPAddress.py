6458b1dc1e9c18398ca1dcca8c4c5b5ee23c7c0a
Atlas Ryder <codehovel@gmail.com>
2011-06-19 15:38:40 -0600

    Creating directory structure

---
+++ src/IPAddress.py
@@ -0,0 +1,20 @@
+class IPAddress:
+  # Construct a new IP Address from the provided bytes
+  def __init__(self, bytes):
+    pass
+  
+  def getByte(index):
+    pass
+
+  # determine if this IP Address is equal to another
+  # Return false (don't raise an exception) if the other
+  # value is not an IP Address
+  def __eq__(other):
+    pass
+
+  # Compare this IP Address to another IP Address.  Raise
+  # An exception if the other value is not an IP Address
+  def __cmp__(other):
+    pass
+
+    
