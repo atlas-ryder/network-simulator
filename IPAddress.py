0bf19c61054327322563dd7aba5037e8531f25dc
Atlas Ryder <codehovel@gmail.com>
2011-06-20 23:09:37 -0600

    Created additional tests, added partial implementation of IPAddress

--- IPAddress.py
+++ IPAddress.py
@@ -1,16 +1,24 @@
+class IPFormatException(BaseException):
+  def __init__(self, msg):
+    self.message = msg
+    
 class IPAddress:
   # Construct a new IP Address from the provided bytes
+
   def __init__(self, bytes):
+    self.validateAndSetBytes(bytes)
+ 
+  def validateAndSetBytes(self, bytes):
     pass
-  
+
   def getByte(self, index):
-    return 1
+    return self.bytes[index]
 
   # determine if this IP Address is equal to another
   # Return false (don't raise an exception) if the other
   # value is not an IP Address
   def __eq__(self, other):
-    return False 
+    return True
 
   # Compare this IP Address to another IP Address.  Raise
   # An exception if the other value is not an IP Address
